ef2d5512c72717ff5fd42f3cd959d21035128081
Kan Sophan <ytbro65@gmail.com>
2023-08-21 22:57:00 +0700

    time to testing

--- app/Contact/contact.tsx
+++ app/Contact/contact.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-gray-900 " id='Contact'>
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
           Contact Us
--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import { FaSearch, FaArrowLeft } from 'react-icons/fa';
 
-export default function search() {
+export default function Search() {
   return (
-  <div>
-    search page
-  </div>
-  )
+    <>
+      <div className="bg-gray-200 ">
+        <div className="container h-screen flex justify-center">
+          <Link href="/"><div className='mt-5 mr-7'><FaArrowLeft /></div></Link>
+          <div className="relative">
+            <div className="absolute top-4 left-5">
+              
+            </div>
+            <input
+              type="text"
+              className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+              placeholder="Search anything..."
+            />
+            <div className="absolute top-2 right-2">
+              <FaSearch />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
